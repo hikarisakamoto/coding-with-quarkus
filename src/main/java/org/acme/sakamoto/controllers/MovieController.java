7a9107bee7ba286ff037fe84ca33066e785c397c
Sakamoto <hikarisakamoto@gmail.com>
2021-06-29 16:20:02 -0300

    Added chat websocket example

--- src/main/java/org/acme/sakamoto/controllers/MovieController.java
+++ src/main/java/org/acme/sakamoto/controllers/MovieController.java
@@ -9,7 +9,14 @@ import org.acme.sakamoto.services.clients.OpenMovieDatabaseClientService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.ByteArrayInputStream;
@@ -69,14 +76,14 @@ public class MovieController {
     }
 
     @POST
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     public String echoFile(String body) {
         return body;
     }
 
-    @Path("test")
     @POST
+    @Path("test")
     @Produces(MediaType.TEXT_PLAIN)
     public String callEcho() {
         final MultipartBody multipartBody = new MultipartBody();
---
+++ src/main/java/org/acme/sakamoto/sockets/MovieChat.java
@@ -0,0 +1,50 @@
+package org.acme.sakamoto.sockets;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.ConcurrentHashMap;
+
+@ApplicationScoped
+@ServerEndpoint(value = "movies/chat/{username}")
+public class MovieChat {
+
+    private final ConcurrentHashMap<String, Session> sessionMap = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        sessionMap.put(username, session);
+        sendMessage(String.format("User %s logged in!\n", username));
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("username") String username) {
+        sendMessage(String.format(" >> %s: %s", username, message));
+    }
+
+    @OnClose
+    public void onClose(@PathParam("username") String username) {
+        sessionMap.remove(username);
+        sendMessage(String.format("User %s logged out!\n", username));
+    }
+
+    @OnError
+    public void onError(@PathParam("username") String username, Throwable throwable) {
+        sessionMap.remove(username);
+        throwable.printStackTrace();
+        sendMessage(String.format("User %s logged out because of an error!\n", username));
+    }
+
+    public void sendMessage(String message) {
+        sessionMap.values().forEach(it -> it.getAsyncRemote().sendObject(message, sendResult -> {
+            if (sendResult.getException() != null) {
+                sendResult.getException().printStackTrace();
+            }
+        }));
+    }
+}
